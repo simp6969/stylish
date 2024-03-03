351c14e8c701b5fd7eb1fd179f072e81a9596848
bold ariunbold <ariunboldbold200@gmail.com>
2024-03-03 20:03:51 +0800

    core files

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <div className="absolute w-[100vw] h-[8vh] flex gap-[20px] justify-end items-center ml-[-35px]">
+      <button>About</button>
+      <button>Documentation</button>
+      <button>Sign</button>
+    </div>
+  );
+}
