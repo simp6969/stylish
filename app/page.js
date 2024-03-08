ab9f80fafab0da71c7de34dc1b93c8c4b900c3cd
bold ariunbold <ariunboldbold200@gmail.com>
2024-03-08 10:56:09 +0800

    audio support

--- app/page.js
+++ app/page.js
@@ -7,6 +7,8 @@ export default function App() {
   const myRefTitle = useRef();
   const [seein, setSeein] = useState(false);
   const [seeinTitle, setSeeinTitle] = useState(false);
+  const [play, setPlay] = useState(false);
+
   useEffect(() => {
     const observer = new IntersectionObserver((e) => {
       const entry = e[0];
@@ -31,6 +33,7 @@ export default function App() {
   return (
     <div className="overflow-hidden">
       <Header />
+      {/* 1st div */}
       <div
         ref={myRefTitle}
         style={
@@ -61,21 +64,21 @@ export default function App() {
           </video>
         </div>
       </div>
-
-      <div
-        style={
-          seein
-            ? {
-                transition: "all 2s",
-                opacity: 1,
-                transform: "translateY(40px)",
-              }
-            : { opacity: 0, transform: "translateY(-40px)" }
-        }
-        className="overflow-hidden h-[100vh] w-[100vw] flex justify-center items-center gap-[30px]"
-      >
-        <h1 className="text-[30px]">About Us</h1>
-        <div className="flex justify-center items-center gap-[20px] h-[30%]">
+      {/* 2nd div */}
+      <div className="overflow-hidden h-[100vh] w-[100vw] flex justify-evenly items-center flex-col gap-[30px]">
+        <h1 className="text-[30px] relative top-0">About Us</h1>
+        <div
+          style={
+            seein
+              ? {
+                  transition: "all 1.5s",
+                  opacity: 1,
+                  transform: "translateX(-200px)",
+                }
+              : { opacity: 0, marginX: "200px" }
+          }
+          className="flex justify-center items-center gap-[20px] h-[30%]"
+        >
           <div className="projects" ref={myRef}>
             <p className="aboutText">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
@@ -124,11 +127,60 @@ export default function App() {
             <p>about Furina</p>
             <p className="aboutText"></p>
           </div>
+          <div className="relative left-[120px] flex flex-col gap-[20px] items-center">
+            <div
+              id="anime"
+              className="cover bg-[url(/theme2.jpg)] bg-cover h-[150px] w-[150px] rounded-[50%]"
+            ></div>
+            <p>Hu tao Cover song</p>
+            <audio id="audio" controls={false}>
+              <source src="/themeSong.mp3" type="audio/mpeg"></source>
+            </audio>
+
+            <div className="flex w-[100%] justify-center gap-[20px]">
+              <svg
+                onClick={() => {
+                  setPlay(!play);
+                  if (!play) {
+                    document.getElementById("anime").style.animationName =
+                      "rotate";
+                  } else
+                    document.getElementById("anime").style.animationName =
+                      "null";
+                }}
+                className="hover:cursor-pointer"
+                height={40}
+                fill="#8c8c8c"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+              >
+                {play ? (
+                  <path
+                    onClick={() => {
+                      document.getElementById("audio").pause();
+                    }}
+                    d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"
+                  />
+                ) : (
+                  <path
+                    onClick={() => {
+                      document.getElementById("audio").play();
+                    }}
+                    d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"
+                  />
+                )}
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
+      {/* 3rd div */}
       <div className="overflow-hidden h-[100vh] w-[100vw] flex justify-center items-center flex-col">
         hi
       </div>
     </div>
   );
+}
+{
+  /* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--></svg> */
 }
